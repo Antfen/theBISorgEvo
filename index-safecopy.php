e9c8bff705cbc746a6158c6d7dec16cf4320120c
Anthony Fenwick <anthonyfenwick@Anthonys-MacBook-Pro.local>
2014-11-28 22:42:03 +0000

    Initial repo build

---
+++ index-safecopy.php
@@ -0,0 +1,73 @@
+<?php include('header.php')
+   ?>
+<style type="text/css">
+   .header_wrapper {
+   display: block;
+   }
+   .header_wrapper_entertainment {
+   display: none;
+   }
+   .header-alt {
+   display: none;
+   }
+   .header {
+   display: block;
+   }
+</style>
+<section class="main clearfix">
+   <div class="row clearfix">
+      <div class="content-holder-40">
+         <span class="mb-center">
+            <p class="para-welcome"><b>Welcome to</b></p>
+            <h2>The British Inventor's Society</h2>
+            <p class="txt-centered">"To create and coordinate the biggest and best R&amp;D department in the world"<br>
+               <b>BIS 2001 - 2014</b>
+            </p>
+            <h2 class="morepadtop">The World Invention Awards 2014</h2>
+            <p>Were held on the 24th October 2014 and hosted at<br>
+               <b>The Savoy Hotel London</b>
+            </p>
+         </span>
+      </div>
+      <div class="content-holder-60">
+         <img src="images/savoy_front.jpg" width="565px" height="378px" alt="The Savoy London">
+      </div>
+   </div>
+   <div class="row-alt clearfix">
+      <div class="content-holder-60"><img src="images/gold_cup_winner.jpg" width="565px" height="377px" alt="The Gold Cup winner">
+      </div>
+      <div class="content-holder-40">
+         <span class="mb-center">
+            <h2>Winner of world’s first King’s BIS Gold Cup for Invention</h2>
+            <p>This year’s winner of The World Invention award is <b>Azizi Miskon</b> of <b>Universiti Pertahanan Nasional Malaysia (UPNM)</b>.</p>
+            <p>This invention consists of a bio reactor and transcutaneous electrical stem simulation system</p>
+            <div class="button-main"><a href="bis-award-winner.php">Read more</a></div>
+         </span>
+      </div>
+   </div>
+   <div class="row clearfix">
+      <div class="content-holder-60">
+         <span class="mb-center">
+            <h2>World’s first Solid Gold Cup for Invention</h2>
+            <h3>King’s bis Gold Cup</h3>
+            <p>To honour exceptional creativity, providing the benchmark on the international stage for innovation and invention. </p>
+         </span>
+      </div>
+      <div class="content-holder-40"><img src="images/gold_cup_globe.jpg" width="324px" height="377px" alt="The Gold Cup winner">
+      </div>
+   </div>
+   <div class="row-alt clearfix">
+      <div class="content-holder-60"><img src="images/entertainment_page_straits.jpg" width="600px" height="400px" alt="The Straits">
+      </div>
+      <div class="content-holder-40">
+         <span class="mb-center">
+            <h2>Entertainment at the Awards's</h2>
+            <h3>Provided by the String Ensemble...</h3>
+            <h3>and by the Straits</h3>
+            <div class="button-main"><a href="entertainment.php">Read more</a></div>
+         </span>
+      </div>
+   </div>
+</section>
+<?php include('footer.php')
+   ?>
